41cb1d90def6f1532835d4b8fc0166a3d3361683
Sean McCreary <mccreary@mcwest.org>
2019-12-20 18:25:01 -0700

    Add input device configuration file for built-in keyboard device
    The hardware keyboard should ignore the device orientation
    
    Change-Id: I41acc4630b668c59985d41b574ff406727d05188

--- common.mk
+++ common.mk
@@ -300,6 +300,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Keyboard
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/Builtin_Keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Builtin_Keyboard.idc
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fpc1020.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/fpc1020.kl \
